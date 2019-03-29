ab372ce13314cee0649f53139447f6c068d32208
tebbini oumayma <oumaymatebbini@email.com>
2019-03-29 22:02:45 +0100

    modification

---
+++ calc.js
@@ -0,0 +1,73 @@
+function un (){
+    var x=document.getelementbyld("un").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
+function deux (){
+    var x=document.getelementbyld("deux").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
+function trois () {
+    var x=document.getelementbyld("trois").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
+function quatre () {
+    var x=document.getelementbyld("quatre").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
+function cinq () {
+    var x=document.getelementbyld("cinq").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
+function six () {
+    var x=document.getelementbyld("six").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
+function sept () {
+    var x=document.getelementbyld("sept").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
+function huit () {
+    var x=document.getelementbyld("huit").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
+function neuf () {
+    var x=document.getelementbyld("neuf").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur+x;
+}
+function egal () {
+    var valeur= document.getelementbyld("resultat").value;
+    var resultatfinal=eval(valeur);
+    document.getelementbyld("resultat").value= resultatfinal;
+}
+function suppr(){
+    var valeur=document.GetElementById("resultat").value;
+var longeur=valeur.length;
+var fin=longeur-1;
+valeur= valeur.substring(0, fin);
+document.getelementbyld("resultat").value=valeur;
+}
+unction c() {
+    document.getElementById("resultat").value="";
+}
+function point() {
+    var x=document.getelementbyld("point").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur + x;
+}
+function multi() {
+    var x=document.getelementbyld("multi").value;
+    var valeur=document.getelementbyld("resultat").value;
+    document.getelementbyld("resultat").value= valeur + x;
+    function divise() {
+        var x=document.getelementbyld("divise").value;
+        var valeur=document.getelementbyld("resultat").value;
+        document.getelementbyld("resultat").value= valeur + x;
